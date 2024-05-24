49b51000cc18756902673b1f79c6eebfcd3ffa06
jcjudkins <jjudkins77@gmail.com>
2024-05-23 23:20:27 -0400

    updating prod

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -2,7 +2,23 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["urchin-app-xeyed.ondigitalocean.app", "jinglepotpublishing.com"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 try:
     from .local import *
 except ImportError:
     pass
+
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=31536000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
